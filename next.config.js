8949b827e7642a2e1adcd0305d83dc1bf5c8e000
Alifi Arzahta <alifiarzahta@gmail.com>
2025-06-18 21:28:25 +0700

    updated

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
+  output: "standalone", // ✅ Ini aman & bermanfaat untuk deploy di Vercel/Docker
 };
 
 module.exports = nextConfig;
