c60816f44bcf96e82dc46fba36789d0d6249c7ad
Alifi Arzahta <alifiarzahta@gmail.com>
2025-06-19 10:38:22 +0700

    update repair

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -18,26 +18,22 @@ declare module "next-auth/jwt" {
 }
 
 export const authOptions: NextAuthOptions = {
-  debug: false,
   adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: "jwt",
   },
-  // Hapus custom cookies bagian state untuk pakai default dari NextAuth
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
       authorization: {
-        params: {
-          prompt: "select_account",
-        },
+        params: { prompt: "select_account" },
       },
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    async session({ token, session }) {
+    async session({ session, token }) {
       if (token) {
         session.user.isAdmin = token.isAdmin;
       }
@@ -45,9 +41,7 @@ export const authOptions: NextAuthOptions = {
     },
     async jwt({ token }) {
       const userInDb = await prisma.user.findUnique({
-        where: {
-          email: token.email!,
-        },
+        where: { email: token.email! },
       });
       token.isAdmin = userInDb?.isAdmin ?? false;
       return token;
